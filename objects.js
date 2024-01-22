050f0ebc469dbeda6cb18722308f6c9810be14fd
Sindhubairavi08 <sindhu10082003@gmail.com>
2024-01-22 23:17:43 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,24 @@
+//method 1
+var movie={
+    "Hinana" : "Yashana",
+    "Fivefeetapart":"Stella"
+}
+console.log(movie)
+movie["Mebeforeyou"]="will"
+console.log(movie)
+
+//method 2
+var details={}
+details["id"]="21ecr194"
+details["name"]="sindhubairavi"
+details["age"]="20"
+console.log(details)
+
+//method 3
+var kec=new Object()
+kec["fees"]="100000"
+kec["accomodation"]="good"
+kec["food"]="pretty decent"
+kec["hostel count"]=10
+console.log(kec)
+console.log(kec["food"])
